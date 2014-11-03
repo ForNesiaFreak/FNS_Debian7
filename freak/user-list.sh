933613017d9139eb08cbabd5f6794a21bc95c84d
ForNesiaFreak <fornesiafreak@gmail.com>
2014-11-04 06:53:04 +0800

    ForNesiaProject
    http://www.fornesia.com

--- freak/user-list.sh
+++ freak/user-list.sh
@@ -15,4 +15,4 @@ JUMLAH="$(awk -F: '$3 >= 1000 && $1 != "nobody" {print $1}' /etc/passwd | wc -l)
 echo "-------------------------------"
 echo "Jumlah akun: $JUMLAH user"
 echo "-------------------------------"
-echo -e "\e[ Script By Yuri Bhuana (fb.com/youree82, 085815002021) ]\e"
+echo -e "\e[ www.fornesia.com ]\e"
