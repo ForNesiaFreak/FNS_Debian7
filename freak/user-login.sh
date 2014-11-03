2b26b3d2b6c2a289277be2ec047b93837a15ad00
ForNesiaFreak <fornesiafreak@gmail.com>
2014-11-03 20:54:37 +0800

    ForNesiaProject, http://www.fornesia.com

---
+++ freak/user-login.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+#
+# Copyright Fedora Computer
+# ==========================
+# 
+
+data=( `ps aux | grep -i dropbear | awk '{print $2}'`);
+
+echo "-----------------------";
+echo "Checking Dropbear login";
+echo "-----------------------";
+
+for PID in "${data[@]}"
+do
+	#echo "check $PID";
+	NUM=`cat /var/log/auth.log | grep -i dropbear | grep -i "Password auth succeeded" | grep "dropbear\[$PID\]" | wc -l`;
+	USER=`cat /var/log/auth.log | grep -i dropbear | grep -i "Password auth succeeded" | grep "dropbear\[$PID\]" | awk '{print $10}'`;
+	IP=`cat /var/log/auth.log | grep -i dropbear | grep -i "Password auth succeeded" | grep "dropbear\[$PID\]" | awk '{print $12}'`;
+	if [ $NUM -eq 1 ]; then
+		echo "$PID - $USER - $IP";
+	fi
+done
+
+echo "";
+
+data=( `ps aux | grep "\[priv\]" | sort -k 72 | awk '{print $2}'`);
+
+echo "----------------------";
+echo "Checking OpenSSH login";
+echo "----------------------";
+
+for PID in "${data[@]}"
+do
+        #echo "check $PID";
+	NUM=`cat /var/log/auth.log | grep -i sshd | grep -i "Accepted password for" | grep "sshd\[$PID\]" | wc -l`;
+	USER=`cat /var/log/auth.log | grep -i sshd | grep -i "Accepted password for" | grep "sshd\[$PID\]" | awk '{print $9}'`;
+	IP=`cat /var/log/auth.log | grep -i sshd | grep -i "Accepted password for" | grep "sshd\[$PID\]" | awk '{print $11}'`;
+        if [ $NUM -eq 1 ]; then
+                echo "$PID - $USER - $IP";
+        fi
+done
+
+echo "";
+
+echo "-------------------";
+echo "Checking PPTP login";
+echo "-------------------";
+last | grep ppp | grep still
+
+echo "";
+
+#Melihat Riwayat Login User
+echo "---------------------------";
+echo "Checking PPTP Login History";
+echo "---------------------------";
+last | grep ppp
+
+echo "";
+
+echo "------------------------------------------------"
+echo " Kalau ada Multi Login Kill Aja "
+echo " Tetap Multi Login Ganti Passnya baru Kill Lagi "
+echo " Caranya pake Kill nomor PID "
+echo "------------------------------------------------"
+
+echo "";
+
+echo "Script By Yuri Bhuana (fb.com/youree82, 085815002021)";
---
+++ null/bench-network.sh
@@ -0,0 +1,1 @@
+wget https://github.com/youree82/debian7/raw/master/bench.sh -O - -o /dev/null|bash
